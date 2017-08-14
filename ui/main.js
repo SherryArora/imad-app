f6e24abf78eb663b81bd05ccb2d0431adfa4e3d5
SherryArora <arora.sherry95@gmail.com>
2017-08-14 14:37:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 alert("hi");
 var ele = document.getElementById('mainn');
 ele.innerHTML='new value';
+var img = document.getElementById('img');
+img.onclick = function(){
+  img.style.marginleft = '100 px';  
+};
