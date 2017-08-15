bfa2c17db0bf855cedf8fe17d24efd6c51b7e1c8
SherryArora <arora.sherry95@gmail.com>
2017-08-15 13:51:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,12 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit =  document.getElementById('submit_btn');
 submit.onclick= function () {
-    var names = ['name1','name2','name3'];
+    if (request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                var names = request.responseText;
+                names = JSON.parse(names);
     var list = '';
     for(var i = 0; i< names.length;i++)
     {
@@ -37,4 +42,11 @@ submit.onclick= function () {
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+            }
+        }
+        
+                                            };
+    request.open('GET','http://arorasherry95.imad.hasura-app.io/submit-name?name=' + name,true);
+    request.send(null);
+    
 };
