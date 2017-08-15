8bedd99abfc2f936e7f9503bf23b68af42c33c1f
SherryArora <arora.sherry95@gmail.com>
2017-08-15 13:56:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,7 @@ button.onclick = function(){
                            
                            
                            //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit =  document.getElementById('submit_btn');
 submit.onclick= function () {
     if (request.readyState === XMLHttpRequest.DONE)
@@ -45,7 +44,8 @@ submit.onclick= function () {
             }
         }
         
-                                            };
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;                                     };
     request.open('GET','http://arorasherry95.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
     
