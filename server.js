2c1bc09c58944ce5146e8c4d340c9c8824b1ee6d
SherryArora <arora.sherry95@gmail.com>
2017-08-15 13:39:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,8 +139,14 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names = [];
-app.get('/submit-name/:name', function (req, res) {
+/*app.get('/submit-name/:name', function (req, res) {
     var name = req.params.name;
+    names.push(name);
+    //json
+  res.send(JSON.stringify(names));
+});*/
+app.get('/submit-name', function (req, res) {
+    var name = req.query.name;
     names.push(name);
     //json
   res.send(JSON.stringify(names));
