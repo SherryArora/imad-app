9a56cd5681e294889ad843ad6c733ccac653c464
SherryArora <arora.sherry95@gmail.com>
2017-08-15 14:22:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ button.onclick = function(){
 var submit =  document.getElementById('submit_btn');
 submit.onclick = function () {
         var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
+    request.onreadystatechange = function() {
     if (request.readyState === XMLHttpRequest.DONE)
         {
             if(request.status === 200)
