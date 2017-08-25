8a3748c88ce98a2a1cd00899e4bc277605abf805
SherryArora <arora.sherry95@gmail.com>
2017-08-25 22:16:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 var config = {
     user: 'arorasherry95',
     database: 'arorasherry95',
@@ -58,6 +59,23 @@ return  htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash (input, salt){
+    
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+    
+}
+
+
+
+app.get('/hash/:input', function (req, res) {
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+  res.sendFile(hashedString);
+});
+
+
+
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
