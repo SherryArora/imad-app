5997095f94ed7b44c5e1ddd1a83c636c1507e954
SherryArora <arora.sherry95@gmail.com>
2017-08-28 16:03:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var Pool = require('pg').Pool;
 var crypto = require('crypto');
 
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'arorasherry95',
@@ -17,6 +18,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000*60*60*24*30}
+}));
 
 
 
@@ -107,6 +112,8 @@ app.post('/login', function (req, res) {
               var salt = dbString.split('$')[2];
               var hashedPassword = hash(password, salt);
               if(hashedPassword === dbString){
+                  // set the session value
+                  req.session.auth = {userId: result.rows[0].id};
                   res.send('correct credentials');
               } else {
            res.send(403).send('username/password is invalid');
@@ -118,7 +125,13 @@ app.post('/login', function (req, res) {
   });
 });
 
-
+app.get('/chech-login', function (req, res) {
+    if(request.session && request.session.auth && request.session.auth.userId )
+    {
+  res.send('You are logged in:' + request.session.auth.userId.toString());}else{
+      res.send('You are not logged in');
+  }
+});
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
