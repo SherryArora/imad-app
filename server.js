6b0d89841dade4b4e0abca3b279f7bd9f443d3d5
SherryArora <arora.sherry95@gmail.com>
2017-09-07 23:26:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.post('/create-user', function (req, res) {
   });
 });
 
-app.post('/login', function (req, res) {
+app.post('/loginn', function (req, res) {
    var username = req.body.username;
    var password = req.body.password;
    
